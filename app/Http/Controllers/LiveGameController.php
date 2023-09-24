548771d06cef5dac49dce02b2c84ff09c3e693ff
gquesnot <gaqu1994@gmail.com>
2023-09-24 17:18:38 +0200

    fix update on live game

--- app/Http/Controllers/LiveGameController.php
+++ app/Http/Controllers/LiveGameController.php
@@ -10,6 +10,7 @@ use App\Models\Summoner;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Inertia\Inertia;
 use Saloon\Exceptions\Request\Statuses\ForbiddenException;
 use Saloon\Exceptions\Request\Statuses\NotFoundException;
@@ -31,29 +32,20 @@ class LiveGameController extends Controller
         $fake_live_game = null;
         $live_game = null;
         $errors = [];
-        try {
 
-            $live_game = $summoner->getLiveGame();
-            $live_game['map'] = Map::find(intval($live_game['mapId']));
-            $live_game['queue'] = Queue::find(intval($live_game['gameQueueConfigId']));
-            $seconds = Carbon::createFromTimestamp($live_game['gameStartTime'] / 1000)->diffInSeconds(now());
-            $live_game['duration'] = gmdate('H:i:s', $seconds);
-            $match_ids = $summoner->summoner_matches()->pluck('match_id');
-            $encounter_counts = $summoner->get_encounters_count_query($match_ids)->pluck('encounter_count', 'summoner_id');
-            foreach ($live_game['participants'] as $key => $participant) {
-                $participant_summoner = Summoner::updateOrCreateSummonerByName($participant['summonerName']);
-                $live_game['participants'][$key]['summoner'] = $participant_summoner;
-                $live_game['participants'][$key]['champion'] = Champion::whereId($participant['championId'])->first();
-                $live_game['participants'][$key]['encounter_count'] = $encounter_counts[$participant_summoner->id] ?? 0;
-            }
+        try {
+            $live_game = $this->getLiveGameData($summoner);
         } catch (NotFoundException|ForbiddenException $e) {
-            if ($lobby_search) {
-                $match_ids = $summoner->summoner_matches()->pluck('match_id');
-                $encounter_counts = $summoner->get_encounters_count_query($match_ids)->pluck('encounter_count', 'summoner_id');
-                $fake_live_game = $summoner->get_live_game_from_lobby_search($lobby_search, $encounter_counts);
-            }
+
         } catch (RateLimitReachedException $e) {
             $errors = ['api' => 'Rate limit reached, please try again later'];
+        }
+
+
+        if ($lobby_search && !$live_game) {
+            $match_ids = $summoner->summoner_matches()->pluck('match_id');
+            $encounter_counts = $summoner->get_encounters_count_query($match_ids)->pluck('encounter_count', 'summoner_id');
+            $fake_live_game = $summoner->get_live_game_from_lobby_search($lobby_search, $encounter_counts);
         }
 
         return Inertia::render('Summoner/LiveGame', [
@@ -61,5 +53,31 @@ class LiveGameController extends Controller
             'fake_live_game' => $fake_live_game,
         ])->with('errors', $errors);
 
+    }
+
+    /**
+     * @throws RateLimitReachedException
+     * @throws ForbiddenException
+     * @throws NotFoundException
+     */
+    private function getLiveGameData(Summoner $summoner)
+    {
+        $live_game = $summoner->getLiveGame();
+        $live_game['map'] = Map::find(intval($live_game['mapId']));
+        $live_game['queue'] = Queue::find(intval($live_game['gameQueueConfigId']));
+        $seconds = Carbon::createFromTimestamp($live_game['gameStartTime'] / 1000)->diffInSeconds(now());
+        $live_game['duration'] = gmdate('H:i:s', $seconds);
+        $match_ids = $summoner->summoner_matches()->pluck('match_id');
+        $encounter_counts = $summoner->get_encounters_count_query($match_ids)->pluck('encounter_count', 'summoner_id');
+        foreach ($live_game['participants'] as $key => $participant) {
+            $participant_summoner = Summoner::whereName($participant['summonerName'])->first();
+            if (!$participant_summoner) {
+                $participant_summoner = Summoner::updateOrCreateSummonerByName($participant['summonerName']);
+            }
+            $live_game['participants'][$key]['summoner'] = $participant_summoner;
+            $live_game['participants'][$key]['champion'] = Champion::whereId($participant['championId'])->first();
+            $live_game['participants'][$key]['encounter_count'] = $encounter_counts[$participant_summoner->id] ?? 0;
+        }
+        return $live_game;
     }
 }
