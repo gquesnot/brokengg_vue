5e5e7fa3519b11d3a7de905e6472203bb6d7f026
gquesnot <gaqu1994@gmail.com>
2023-10-01 22:52:05 +0200

    fix champions pagination

--- resources/js/Pages/Summoner/Champions.vue
+++ resources/js/Pages/Summoner/Champions.vue
@@ -20,39 +20,41 @@ const params_to_reload: string[] = [
         <SummonerHeader
             tab="Champions"
         />
-
-        <VTable class="position-absolute w-10/12 left-40">
-            <thead>
-            <tr>
-                <th class="text-left">Champion</th>
-                <th class="text-left">W-L</th>
-                <th class="text-left">Win Rate</th>
-                <th class="text-left">Avg KDA</th>
-                <th class="text-left">Max Kills</th>
-                <th class="text-left">Max Deaths</th>
-                <th class="text-left">Max Assists</th>
-                <th class="text-left">Avg CS</th>
-                <th class="text-left">Avg Damage Dealt to Champions</th>
-                <th class="text-left">Avg Damage Taken</th>
-                <th class="text-left">Avg Gold</th>
-                <th class="text-left">Total Double Kills</th>
-                <th class="text-left">Total Triple Kills</th>
-                <th class="text-left">Total Quadra Kills</th>
-                <th class="text-left">Total Penta Kills</th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="(champion, idx) in champions.data" :key="champion.id"
-                :class="(idx % 2 === 0 ? 'bg-zinc-800' : '' ) + ' hover:bg-zinc-900'">
-                <ChampionsRow
-                    :champion="champion"
-                    :key="champion.id"
-                    version=""/>
-            </tr>
-            </tbody>
+      <div class="position-absolute w-10/12 left-40 my-4">
+        <VTable>
+          <thead>
+          <tr>
+            <th class="text-left">Champion</th>
+            <th class="text-left">W-L</th>
+            <th class="text-left">Win Rate</th>
+            <th class="text-left">Avg KDA</th>
+            <th class="text-left">Max Kills</th>
+            <th class="text-left">Max Deaths</th>
+            <th class="text-left">Max Assists</th>
+            <th class="text-left">Avg CS</th>
+            <th class="text-left">Avg Damage Dealt to Champions</th>
+            <th class="text-left">Avg Damage Taken</th>
+            <th class="text-left">Avg Gold</th>
+            <th class="text-left">Total Double Kills</th>
+            <th class="text-left">Total Triple Kills</th>
+            <th class="text-left">Total Quadra Kills</th>
+            <th class="text-left">Total Penta Kills</th>
+            <th></th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="(champion, idx) in champions.data" :key="champion.id"
+              :class="(idx % 2 === 0 ? 'bg-zinc-800' : '' ) + ' hover:bg-zinc-900'">
+            <ChampionsRow
+                :champion="champion"
+                :key="champion.id"
+                version=""/>
+          </tr>
+          </tbody>
         </VTable>
         <Pagination :links="champions.links"/>
+      </div>
+
 
     </div>
 
