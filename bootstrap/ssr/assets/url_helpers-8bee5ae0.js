0cfe0588d03bffc026a128b193d48a00f870c832
gquesnot <gaqu1994@gmail.com>
2023-11-20 15:38:48 +0100

    remove

--- bootstrap/ssr/assets/url_helpers-8bee5ae0.js
+++
@@ -1,8 +0,0 @@
-const urlChampionHelper = (version, img_url) => `https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${img_url}`;
-const urlItemHelper = (version, img_url) => `https://ddragon.leagueoflegends.com/cdn/${version}/img/item/${img_url}`;
-const urlProfilIconHelper = (version, img_id) => `https://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon/${img_id.toString()}.png`;
-export {
-  urlItemHelper as a,
-  urlProfilIconHelper as b,
-  urlChampionHelper as u
-};
