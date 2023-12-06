5fb332f7ec20bf79f2cadd3bdcc291840988c680
gquesnot <gaqu1994@gmail.com>
2023-12-06 19:35:09 +0100

    fix limit

--- app/Console/Commands/UpdateSummonerTagLineCommand.php
+++ app/Console/Commands/UpdateSummonerTagLineCommand.php
@@ -14,22 +14,16 @@ class UpdateSummonerTagLineCommand extends Command
 
     public function handle(): void
     {
-        $next_now = Carbon::now()->addMinute();
-        $count = 0;
         foreach (Summoner::where('tag_line', '=', null)->cursor() as $summoner) {
-            $account = Summoner::getAccountByPuuid($summoner->puuid);
+            try {
+                $account = Summoner::getAccountByPuuid($summoner->puuid);
+            } catch (\Exception $e) {
+                sleep(60);
+                $account = Summoner::getAccountByPuuid($summoner->puuid);
+            }
             $summoner->tag_line = $account['tagLine'];
             $summoner->name = $account['gameName'];
             $summoner->save();
-            $count++;
-            if ($count >= 500) {
-                $count = 0;
-                $now = Carbon::now();
-                if (($next_now->greaterThan($now))) {
-                    sleep($now->diffInSeconds($next_now));
-                }
-                $next_now = Carbon::now()->addMinute();
-            }
         }
     }
 }
