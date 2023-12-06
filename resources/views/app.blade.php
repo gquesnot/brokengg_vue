b66ca16341b9e8a5de5e369032f12c57fc9af967
gquesnot <gaqu1994@gmail.com>
2023-12-06 09:04:17 +0100

    analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RWHSG7KWHV"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+
+            gtag('js', new Date());
+
+            gtag('config', 'G-RWHSG7KWHV');
+        </script>
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
